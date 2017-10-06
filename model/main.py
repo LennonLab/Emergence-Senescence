17eee2b083d40daeb5ce2409f7d9a5e26a1e0c8b
RoyBoy432 <rmogerr2@u.rochester.edu>
2017-10-06 09:05:41 -0400

    Fixed one more accidental change to the implemented model. -RZM

--- model/main.py
+++ model/main.py
@@ -123,7 +123,6 @@ def reproduce(sD, iD, ps, p = 0):
     for k, v in iD.items():
         if v['q'] > v['mt']*2 and np.random.binomial(1, v['gr']) == 1:
             iD[k]['q'] = v['q']/2.0
-            iD[k]['age']+=1
             i = time.time()
             iD[i] = copy.copy(iD[k])
     return [sD, iD]
